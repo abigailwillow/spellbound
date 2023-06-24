87c252dec00a49b532805c1f5a2e4fe6ef7ad361
abbydiode <contact@abbydiode.com>
2023-06-24 16:17:04 +0200

    Add tutorial for word relations
    #25

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -267,6 +267,8 @@ public class GameManager : MonoBehaviourPunCallbacks {
                     "Welcome to Spellbound!",
                     "In this game players take turns spelling words to cast spells on each other",
                     "The longer the word, and less common the letters, the more damage your spell will inflict",
+                    "Words with relations to other words have special effects",
+                    "Synonyms heal you\nAntonyms deflect incoming spells\nRelated words amplify your spells",
                     "You can type EXIT during a game to return to the menu",
                     "Type SPELLBOUND to get started"
                 );
